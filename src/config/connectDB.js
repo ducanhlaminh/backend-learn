0ae57581b5cb26999038da3136bb5cc109da5ed9
ducanhlaminh <89836104+ducanhlaminh@users.noreply.github.com>
2023-05-01 22:27:39 +0700

    comit

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,18 +1,41 @@
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
-// Option 3: Passing parameters separately (other dialects)
+// // Option 3: Passing parameters separately (other dialects)
+// const sequelize = new Sequelize("database_zing", "root", null, {
+//         host: "localhost",
+//         dialect: "mysql",
+//         logging: false,
+// });
+
+// const connectDB = async () => {
+//         try {
+//                 await sequelize.authenticate();
+//                 console.log("Connection to database successfully.");
+//         } catch (error) {
+//                 console.error("Unable to connect to the database:", error);
+//         }
+// };
+// connectDB();
+
+const Sequelize = require("sequelize");
+const mariadb = require("mariadb");
+
 const sequelize = new Sequelize("database_zing", "root", null, {
-        host: "localhost",
-        dialect: "mysql",
-        logging: false,
+      dialect: "mariadb",
+      host: "localhost",
+      pool: {
+            max: 5,
+            min: 0,
+            idle: 10000,
+      },
+      logging: false,
 });
 
-const connectDB = async () => {
-        try {
-                await sequelize.authenticate();
-                console.log("Connection to database successfully.");
-        } catch (error) {
-                console.error("Unable to connect to the database:", error);
-        }
-};
-connectDB();
+sequelize
+      .authenticate()
+      .then(() => {
+            console.log("Connected to MariaDB!");
+      })
+      .catch((err) => {
+            console.log("Error connecting to MariaDB: " + err);
+      });
