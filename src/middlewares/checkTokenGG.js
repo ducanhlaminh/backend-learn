4beb461936d307ca4eb9a5c84081b199def9627b
ducanhlaminh <89836104+ducanhlaminh@users.noreply.github.com>
2023-06-26 00:58:30 +0700

    cm

--- src/middlewares/checkTokenGG.js
+++ src/middlewares/checkTokenGG.js
@@ -3,32 +3,28 @@ const {
     OAuth2Strategy: GoogleOAuth2Strategy,
 } = require("passport-google-oauth20");
 
-passport.use(
-    new GoogleOAuth2Strategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID, // Thay YOUR_CLIENT_ID bằng Client ID của ứng dụng Google
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Thay YOUR_CLIENT_SECRET bằng Client Secret của ứng dụng Google
-        },
-        (accessToken, refreshToken, profile, done) => {
-            // Kiểm tra tính hợp lệ của accessToken
-            if (accessTokenIsValid(accessToken)) {
-                // Access token hợp lệ
-                return done(null, profile);
-            } else {
-                // Access token không hợp lệ
-                return done(null, false);
+const checkTokenGG = (req, res, next) => {
+    passport.use(
+        new GoogleOAuth2Strategy(
+            {
+                clientID: process.env.GOOGLE_CLIENT_ID, // Thay YOUR_CLIENT_ID bằng Client ID của ứng dụng Google
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Thay YOUR_CLIENT_SECRET bằng Client Secret của ứng dụng Google
+            },
+            (accessToken, refreshToken, profile, done) => {
+                // Kiểm tra tính hợp lệ của accessToken
+                if (accessToken === localStorage.getItem("token")) {
+                    // Access token hợp lệ
+                    console.log(profile);
+                    next();
+                } else {
+                    // Access token không hợp lệ
+                    return res.status(401).json({
+                        status: -1,
+                        message: "Verify Failed GG!!!",
+                    });
+                }
             }
-        }
-    )
-);
-
-function accessTokenIsValid(accessToken) {
-    try {
-        const decodedToken = jwt.verify(accessToken, secretKey);
-        // Nếu không xảy ra lỗi khi xác minh chữ ký, access token được coi là hợp lệ
-        return true;
-    } catch (error) {
-        // Xử lý lỗi xác minh chữ ký
-        return false;
-    }
-}
+        )
+    );
+};
+module.exports = checkTokenGG;
