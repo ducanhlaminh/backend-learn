41aa9d2150289dc1512a91b801d70701736eda95
ducanhlaminh <89836104+ducanhlaminh@users.noreply.github.com>
2023-06-29 23:57:04 +0700

    c

--- src/modules/users/routes/auth.js
+++ src/modules/users/routes/auth.js
@@ -7,22 +7,24 @@ const authController = require("../userControllers/authControllers");
 authRoutes.post("/register", authController.registerController);
 authRoutes.post("/login", authController.loginController);
 authRoutes.get(
-    "/google",
-    passport.authenticate("google", { scope: ["profile", "email"] })
+      "/google",
+      passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 authRoutes.get(
-    "/google/callback",
-    (req, res, next) => {
-        passport.authenticate("google", (err, profile) => {
-            req.user = profile;
-            next();
-        })(req, res, next);
-    },
-    (req, res) => {
-        // Successful authentication, redirect home.
-        res.redirect(process.env.CLIENT_URL + "/login-success/" + req.user?.id);
-    }
+      "/google/callback",
+      (req, res, next) => {
+            passport.authenticate("google", (err, profile) => {
+                  req.user = profile;
+                  next();
+            })(req, res, next);
+      },
+      (req, res) => {
+            // Successful authentication, redirect home.
+            res.redirect(
+                  process.env.CLIENT_URL + "/login-success/" + req.user?.id
+            );
+      }
 );
 authRoutes.get("/login-success/:id", authController.loginSuccessController);
 module.exports = authRoutes;
