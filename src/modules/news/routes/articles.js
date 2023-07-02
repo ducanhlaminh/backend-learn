ee9693dd80316278135b7a1f23376b5f191a3f69
Ducreus <ducanhlaminh@gmail.com>
2023-07-02 08:58:38 +0700

    c

--- src/modules/news/routes/articles.js
+++ src/modules/news/routes/articles.js
@@ -1,60 +1,73 @@
 const express = require("express");
 const articlesRoutes = express.Router();
-const { checkToken } = require("../../../middlewares/checkToken");
+const { checkToken, checkManager } = require("../../../middlewares/checkToken");
 const articlesController = require("../newControllers/articlesController");
 // articlesRoutes.use(checkToken);
 articlesRoutes.get(
-    "/hot-main",
-    articlesController.get_articles.getHotControllers
+      "/hot-main",
+      articlesController.get_articles.getHotControllers
 );
 articlesRoutes.get(
-    "/cate/:slug/:slug_crc",
-    articlesController.get_articles.getByCateControllers
+      "/cate/:slug/:slug_crc",
+      articlesController.get_articles.getByCateControllers
 );
 articlesRoutes.get(
-    "/hot-cate/",
-    articlesController.get_articles.getHotCategoryController
+      "/hot-cate/",
+      articlesController.get_articles.getHotCategoryController
 );
 articlesRoutes.get(
-    "/publish_at",
-    articlesController.get_articles.getByPublishAtController
+      "/publish_at",
+      articlesController.get_articles.getByPublishAtController
 );
 articlesRoutes.get(
-    "/:slug/:slug_crc",
-    articlesController.get_articles.getDetailControllers
+      "/:slug/:slug_crc",
+      articlesController.get_articles.getDetailControllers
 );
 articlesRoutes.get(
-    "/views",
-    articlesController.get_articles.getByViewsController
+      "/views",
+      articlesController.get_articles.getByViewsController
 );
 
 articlesRoutes.get(
-    "/title",
-    articlesController.get_articles.getByTitleControllers
+      "/title",
+      articlesController.get_articles.getByTitleControllers
 );
 articlesRoutes.post(
-    "/",
-    articlesController.create_articles.createArticleControllers
+      "/",
+      checkManager,
+      articlesController.create_articles.createArticleControllers
 );
 articlesRoutes.post(
-    "/hot-main",
-    articlesController.create_articles.createHotMainController
+      "/hot-main",
+      checkManager,
+      articlesController.create_articles.createHotMainController
 );
 articlesRoutes.post(
-    "/hot-cate",
-    articlesController.create_articles.createHotCateController
+      "/hot-cate",
+      checkManager,
+      articlesController.create_articles.createHotCateController
 );
 
 articlesRoutes.put(
-    "/hot-main/:id",
-    articlesController.update_articles.updateHotMainController
+      "/hot-main/:id",
+      checkManager,
+      articlesController.update_articles.updateHotMainController
 );
 articlesRoutes.put(
-    "/:id",
-    articlesController.update_articles.publishController
+      "/:id",
+      checkManager,
+      articlesController.update_articles.publishController
 );
 
 // insert data
-articlesRoutes.get("/insert", articlesController.insert.insertData);
-articlesRoutes.get("/publishAll", articlesController.insert.publishData);
+articlesRoutes.get(
+      "/insert",
+      checkManager,
+      articlesController.insert.insertData
+);
+articlesRoutes.get(
+      "/publishAll",
+      checkManager,
+      articlesController.insert.publishData
+);
 module.exports = articlesRoutes;
