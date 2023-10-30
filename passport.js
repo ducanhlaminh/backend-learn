2791d07f4accfdab146e26e711576fe6668ec8d1
Ducreus <ducanhlaminh@gmail.com>
2023-10-30 08:49:34 +0700

    123

--- passport.js
+++ passport.js
@@ -4,37 +4,31 @@ const db = require("./src/config/userModels");
 var GoogleStrategy = require("passport-google-oauth20").Strategy;
 const passport = require("passport");
 passport.use(
-        new GoogleStrategy(
-                {
-                        clientID: process.env.GOOGLE_CLIENT_ID,
-                        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                        callbackURL:
-                                "http://localhost:4000/api/v1/auth/google/callback",
-                },
-                async function (accessToken, refreshToken, profile, cb) {
-                        try {
-                                console.log(profile);
-                                const [user, created] =
-                                        await db.User.findOrCreate({
-                                                where: {
-                                                        email: profile
-                                                                ?.emails[0]
-                                                                ?.value,
-                                                },
-                                                defaults: {
-                                                        email: profile
-                                                                ?.emails[0]
-                                                                ?.value,
-                                                        typeLogin: 1,
-                                                        name: profile.displayName,
-                                                        password: "admin123",
-                                                },
-                                        });
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:4000/api/v1/auth/google/callback",
+        },
+        async function (accessToken, refreshToken, profile, cb) {
+            try {
+                console.log(profile);
+                const [user, created] = await db.User.findOrCreate({
+                    where: {
+                        email: profile?.emails[0]?.value,
+                    },
+                    defaults: {
+                        email: profile?.emails[0]?.value,
+                        typeLogin: 1,
+                        name: profile.displayName,
+                        password: "admin123",
+                    },
+                });
 
-                                return cb(null, { user, profile });
-                        } catch (error) {
-                                console.log(error);
-                        }
-                }
-        )
+                return cb(null, { user, profile });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    )
 );
--- src/config/newModels/new_category.js
+++ src/config/newModels/new_category.js
@@ -1,48 +1,48 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-        class new_category extends Model {
-                /**
-                 * Helper method for defining associations.
-                 * This method is not a part of Sequelize lifecycle.
-                 * The `models/index` file will call this method automatically.
-                 */
-                static associate(models) {
-                        new_category.hasMany(models.new_category, {
-                                foreignKey: "parent_id",
-                                as: "childCategories",
-                        });
-                        new_category.belongsTo(models.new_category, {
-                                foreignKey: "parent_id",
-                                as: "parentCategory",
-                        });
-                        new_category.hasMany(models.new_articles_category, {
-                                foreignKey: "category_id",
-                                as: "articles",
-                        });
-                        new_category.hasMany(models.new_articles_hot_category, {
-                                foreignKey: "category_id",
-                        });
-                        // define association here
-                }
+    class new_category extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            new_category.hasMany(models.new_category, {
+                foreignKey: "parent_id",
+                as: "childCategories",
+            });
+            new_category.belongsTo(models.new_category, {
+                foreignKey: "parent_id",
+                as: "parentCategory",
+            });
+            new_category.hasMany(models.new_articles_category, {
+                foreignKey: "category_id",
+                as: "articles",
+            });
+            new_category.hasMany(models.new_articles_hot_category, {
+                foreignKey: "category_id",
+            });
+            // define association here
         }
-        new_category.init(
-                {
-                        name: DataTypes.STRING,
-                        slug: DataTypes.STRING,
-                        slug_crc: DataTypes.BIGINT,
-                        parent_id: DataTypes.INTEGER,
-                        position: DataTypes.INTEGER,
-                        status: DataTypes.INTEGER,
-                        updated_user_id: DataTypes.INTEGER,
-                        created_user_id: DataTypes.INTEGER,
-                        updatedAt: DataTypes.DATE,
-                        createdAt: DataTypes.DATE,
-                },
-                {
-                        sequelize,
-                        modelName: "new_category",
-                }
-        );
-        return new_category;
+    }
+    new_category.init(
+        {
+            name: DataTypes.STRING,
+            slug: DataTypes.STRING,
+            slug_crc: DataTypes.BIGINT,
+            parent_id: DataTypes.INTEGER,
+            position: DataTypes.INTEGER,
+            status: DataTypes.INTEGER,
+            updated_user_id: DataTypes.INTEGER,
+            created_user_id: DataTypes.INTEGER,
+            updatedAt: DataTypes.DATE,
+            createdAt: DataTypes.DATE,
+        },
+        {
+            sequelize,
+            modelName: "new_category",
+        }
+    );
+    return new_category;
 };
